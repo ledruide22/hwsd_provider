900a758ec73de3c59e85bfea27db1997cb101823
Olivier D <olivier.dauloudet@hotmail.com>
2022-04-02 20:55:05 +0200

    complete setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,26 @@
 # -*- coding: utf-8 -*-
-
 from setuptools import setup, find_packages
 
 with open('README.md') as f:
     readme = f.read()
 
 with open('LICENSE') as f:
-    license = f.read()
+    my_license = f.read()
 
 setup(
     name='hwsd_provider',
-    version='0.1.1',
+    version='0.1.0',
     description='hwsd soil data provider',
     long_description=readme,
     author='Dauloudet Olivier',
     url='https://github.com/Smeaol22/hwsd_provider.git',
-    license=license,
+    license=my_license,
+    install_requires=[
+        "gdal>=3.4,<3.5",
+        "pyodbc>=4.0,<4.1",
+        "rasterio>=1.2,<1.3",
+        "jaydebeapi>=1.2,<1.3"
+    ],
     package_dir={'': 'src'},
     packages=find_packages('src')
 )
