dfb934ff7b1124845630eb44f042508ca5a1f5d3
Olivier D <olivier.dauloudet@hotmail.com>
2022-04-02 21:10:57 +0200

    fix version for linux

--- setup.py
+++ setup.py
@@ -16,9 +16,9 @@ setup(
     url='https://github.com/Smeaol22/hwsd_provider.git',
     license=my_license,
     install_requires=[
-        "gdal>=3.4,<3.5",
+        "gdal>=3.0,<3.5",
         "pyodbc>=4.0,<4.1",
-        "rasterio>=1.2,<1.3",
+        "rasterio>=1.1,<1.3",
         "jaydebeapi>=1.2,<1.3"
     ],
     package_dir={'': 'src'},
